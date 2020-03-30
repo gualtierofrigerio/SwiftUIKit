8bca3dbf8ac1aac696b49a677b44e3a014fcceee
Gualtiero Frigerio <gualtiero.frigerio@gmail.com>
2020-03-30 15:20:04 +0200

    refactoring and added ZStack

--- SwiftUIKit/SwiftUIKitStack.swift
+++ SwiftUIKit/SwiftUIKitStack.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-struct HStack: SwiftUIKitStack {
+struct HStack:SwiftUIKitView {
     var type: SwiftUIKitViewType {
         .single(containerView)
     }
@@ -21,65 +21,81 @@ struct HStack: SwiftUIKitStack {
         let uiKitView = builder()
         containerView = UIView(frame: frame)
         if let uiViews = uiKitView.type.uiViews() {
-            addViews(uiViews, align: .horizontal, toView: containerView)
+            addViews(uiViews, toView: containerView)
         }
     }
     
     private var containerView:UIView
-}
-
-struct VStack: SwiftUIKitStack {
-    var type: SwiftUIKitViewType {
-        .single(containerView)
-    }
-    var uiView: UIView? {
-        containerView
-    }
     
-    init(frame:CGRect, @SwiftUIKitViewBuilder _ builder:()->SwiftUIKitView) {
-        let uiKitView = builder()
-        containerView = UIView(frame: frame)
-        if let uiViews = uiKitView.type.uiViews() {
-            addViews(uiViews, align: .vertical, toView: containerView)
-        }
-    }
-    
-    private var containerView:UIView
-}
-
-private enum SwiftUIKitAlignment {
-    case horizontal
-    case vertical
-    case none
-}
-
-private protocol SwiftUIKitStack:SwiftUIKitView {
-    
-}
-
-extension SwiftUIKitStack {
-    func addViews(_ views:[UIView], align:SwiftUIKitAlignment, toView containerView:UIView) {
+    private func addViews(_ views: [UIView], toView containerView: UIView) {
         let size = containerView.frame.size
-        var height = size.height
-        var width = size.width
-        var xStep:CGFloat = 0.0
-        var yStep:CGFloat = 0.0
-        if align == .horizontal {
-            width = size.width / CGFloat(views.count)
-            xStep = width
-        }
-        else {
-            height = size.height / CGFloat(views.count)
-            yStep = height
-        }
-        var frame = CGRect(x: 0, y: 0, width: width, height: height)
+        let width = size.width / CGFloat(views.count)
+        
+        var frame = CGRect(x: 0, y: 0, width: width, height: size.height)
         for view in views {
             view.frame = frame
             containerView.addSubview(view)
-            frame.origin.x += xStep
-            frame.origin.y += yStep
+            frame.origin.x += width
         }
     }
 }
 
+struct VStack: SwiftUIKitView {
+    var type: SwiftUIKitViewType {
+        .single(containerView)
+    }
+    var uiView: UIView? {
+        containerView
+    }
+    
+    init(frame:CGRect, @SwiftUIKitViewBuilder _ builder:()->SwiftUIKitView) {
+        let uiKitView = builder()
+        containerView = UIView(frame: frame)
+        if let uiViews = uiKitView.type.uiViews() {
+            addViews(uiViews, toView: containerView)
+        }
+    }
+    
+    private var containerView:UIView
+    
+    private func addViews(_ views: [UIView], toView containerView: UIView) {
+        let size = containerView.frame.size
+        let height = size.height / CGFloat(views.count)
+        
+        var frame = CGRect(x: 0, y: 0, width: size.width, height: height)
+        for view in views {
+            view.frame = frame
+            containerView.addSubview(view)
+            frame.origin.y += height
+        }
+    }
+}
 
+struct ZStack: SwiftUIKitView {
+    var type: SwiftUIKitViewType {
+        .single(containerView)
+    }
+    var uiView: UIView? {
+        containerView
+    }
+    
+    init(frame:CGRect, @SwiftUIKitViewBuilder _ builder:()->SwiftUIKitView) {
+        let uiKitView = builder()
+        containerView = UIView(frame: frame)
+        if let uiViews = uiKitView.type.uiViews() {
+            addViews(uiViews, toView: containerView)
+        }
+    }
+    
+    private var containerView:UIView
+    
+    private func addViews(_ views: [UIView], toView containerView: UIView) {
+        var zPosition:CGFloat = 0.0
+        
+        for view in views {
+            containerView.addSubview(view)
+            view.layer.zPosition = zPosition
+            zPosition += 1
+        }
+    }
+}
